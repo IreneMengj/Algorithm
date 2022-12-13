2b3b6b07f02004d510bed937740dc7daa0972d46
irene <mengjiayu2021@gmail.com>
2022-12-13 11:16:54 +0000

    Stack

---
+++ src/linear/Stack.java
@@ -0,0 +1,47 @@
+package linear;
+
+import sun.tools.tree.Node;
+
+public class Stack<T> {
+    private Node head;
+    //the number of elements
+    private int N;
+
+    private class Node{
+        public T item;
+        public Node next;
+        public Node(T item, Node next){
+            this.item=item;
+            this.next=next;
+        }
+    }
+    public Stack(){
+        this.head=new Node(null,null);
+        this.N=0;
+    }
+    public boolean isEmpty(){
+        return N==0;
+    }
+    public int size(){
+        return N;
+    }
+    //push onto stack
+    public void push(T t){
+        Node oldFirst = head.next;
+        Node newNode = new Node(t, null);
+        head.next=newNode;
+        newNode.next=oldFirst;
+        N++;
+    }
+
+    public T pop(){
+        Node oldFirst = head.next;
+        if(oldFirst==null){
+            return null;
+        }
+        head.next=oldFirst.next;
+        N--;
+        return null;
+    }
+
+}
